f2792a596de52b612bafbfb3f55df31978d10117
Nidhi-Sharma-csit <nidhi1912002@akgec.ac.in>
2020-09-21 20:00:07 +0530

    added urls for refresh token and login token using simplejwt

--- api/api/settings.py
+++ api/api/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_auth',
-    'rest_framework.authtoken',
+    # 'rest_auth',
+    # 'rest_framework.authtoken',
     'userauth'
 ]
 
@@ -136,39 +136,11 @@ REST_FRAMEWORK = {
          'rest_framework.permissions.IsAdminUser',
          ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-     'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+     'rest_framework_simplejwt.authentication.JWTAuthentication',
      )
 }
-REST_USE_JWT = True
+
 from datetime import timedelta
-JWT_AUTH = {
-  'JWT_ENCODE_HANDLER':
-  'rest_framework_jwt.utils.jwt_encode_handler',
-  'JWT_DECODE_HANDLER':
-  'rest_framework_jwt.utils.jwt_decode_handler',
-  'JWT_PAYLOAD_HANDLER':
-  'rest_framework_jwt.utils.jwt_payload_handler',
-  'JWT_PAYLOAD_GET_USER_ID_HANDLER':
-  'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
-  'JWT_RESPONSE_PAYLOAD_HANDLER':
-  'rest_framework_jwt.utils.jwt_response_payload_handler',
- 
-  'JWT_SECRET_KEY': 'SECRET_KEY',
-  'JWT_GET_USER_SECRET_KEY': None,
-  'JWT_PUBLIC_KEY': None,
-  'JWT_PRIVATE_KEY': None,
-  'JWT_ALGORITHM': 'HS256',
-  'JWT_VERIFY': True,
-  'JWT_VERIFY_EXPIRATION': True,
-  'JWT_LEEWAY': 0,
-  'JWT_EXPIRATION_DELTA': timedelta(days=30),
-  'JWT_AUDIENCE': None,
-  'JWT_ISSUER': None,
-  'JWT_ALLOW_REFRESH': False,
-  'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=30),
-  'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-  'JWT_AUTH_COOKIE': None,
-}
 
 #Email Notifications
 EMAIL_HOST = 'smtp.gmail.com'
--- api/userauth/urls.py
+++ api/userauth/urls.py
@@ -1,17 +1,20 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
+from django.urls import path,include
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 from rest_framework import routers
 from userauth.views import UserViewSet
-
+from rest_framework_simplejwt import views as jwt_views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^auth/', include('rest_auth.urls'))
+    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
